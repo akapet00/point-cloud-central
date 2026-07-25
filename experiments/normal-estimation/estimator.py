14249637995622bb63dc1ad3adbb9dd15a230381
akapet00 <akapet00@gmail.com>
2026-07-25 21:42:30 +0200

    Experiment 137: Extend only the first Tukey-PCA refinement from 112 to 128 neighbors

--- experiments/normal-estimation/estimator.py
+++ experiments/normal-estimation/estimator.py
@@ -9,6 +9,7 @@ from __future__ import annotations
 import numpy as np
 
 NEIGHBOR_COUNT = 112
+FIRST_REFINEMENT_COUNT = 128
 FIRST_STATISTIC_COUNT = 32
 FINAL_NEIGHBOR_COUNT = 128
 FINAL_STATISTIC_COUNT = 32
@@ -81,7 +82,7 @@ def estimate_normals(
         normals = eigenvectors[:, :, 0]
 
         scale_floor = np.finfo(np.float64).eps * np.maximum(bandwidth, 1.0)
-        refinement_counts = (NEIGHBOR_COUNT, FINAL_NEIGHBOR_COUNT)
+        refinement_counts = (FIRST_REFINEMENT_COUNT, FINAL_NEIGHBOR_COUNT)
         statistic_counts = (FIRST_STATISTIC_COUNT, FINAL_STATISTIC_COUNT)
         for tukey_cutoff, refinement_count, statistic_count in zip(
             TUKEY_CUTOFFS, refinement_counts, statistic_counts, strict=True
