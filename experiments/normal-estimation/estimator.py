e037e26c34dc6b360b9c698faf17c3aa76139ca8
akapet00 <akapet00@gmail.com>
2026-07-26 09:53:54 +0200

    Experiment 294: Blend 0.15625% of the broad provisional PCA normal into non-thin local patches

--- experiments/normal-estimation/estimator.py
+++ experiments/normal-estimation/estimator.py
@@ -17,6 +17,7 @@ DISTANCE_DECAY = 2.0
 TUKEY_CUTOFFS = (4.15, 2.77, 2.77)
 THIRD_REFINEMENT_MAX_THICKNESS = 0.1
 THIRD_NORMAL_EXTRAPOLATION = 0.8
+THICK_SHEET_BROAD_NORMAL_BLEND = 0.0015625
 MAD_TO_SIGMA = 1.4826
 BATCH_SIZE = 2_048
 
@@ -80,6 +81,7 @@ def estimate_normals(
         )
         _, eigenvectors = np.linalg.eigh(covariance)
         normals = eigenvectors[:, :, 0]
+        broad_normals = normals.copy()
 
         scale_floor = np.finfo(np.float64).eps * np.maximum(bandwidth, 1.0)
         refinement_counts = (
@@ -133,7 +135,16 @@ def estimate_normals(
                     aligned_normals - normals
                 )
                 extrapolated /= np.linalg.norm(extrapolated, axis=1, keepdims=True)
-                normals = np.where(thin_sheet, extrapolated, normals)
+
+                broad_dot = np.einsum(
+                    "ni,ni->n", broad_normals, normals, optimize=True
+                )[:, None]
+                aligned_broad = broad_normals * np.where(broad_dot < 0.0, -1.0, 1.0)
+                broad_blend = (
+                    1.0 - THICK_SHEET_BROAD_NORMAL_BLEND
+                ) * normals + THICK_SHEET_BROAD_NORMAL_BLEND * aligned_broad
+                broad_blend /= np.linalg.norm(broad_blend, axis=1, keepdims=True)
+                normals = np.where(thin_sheet, extrapolated, broad_blend)
             else:
                 normals = refined_normals
 
